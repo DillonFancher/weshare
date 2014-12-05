346f9e2bfaeba4bce657659934d66710709001dc
DillonFancher <daffancher@gmail.com>
2014-12-05 11:06:04 -0700

    df: thought this commit went through, command line args specify two lat lon points.

--- src/main/scala/SessionController.scala
+++ src/main/scala/SessionController.scala
@@ -4,10 +4,10 @@ object SessionController {
    
   def main(args: Array[String]) {
     println("We are going to make this happen!")
-    val start: Boundary = Boundary(-90.05, 29.9667)
-    val inBoundary: Boolean = start.withinBoundary(-90.485, 30.1750)
-    if(inBoundary) println("You should get a picture!")
-    else println("You are no longer in the session")
+    val start: Boundary = Boundary(args(0).toDouble, args(1).toDouble)
+    val inBoundary: (Boolean, Double) = start.withinBoundary(args(2).toDouble, args(3).toDouble)
+    if(inBoundary._1) println("You should get a picture, because you are "+ inBoundary._2+ "away from LIFE!")
+    else println("You are no longer in the session, because you are " + inBoundary._2+ " miles away from DEATH:!")
   }
 
   def geoLocator(geoCoordinates: (Double, Double)): Boolean = {
@@ -17,7 +17,7 @@ object SessionController {
   case class Boundary(initialGeoCoord: (Double, Double)) {    val lat1 = math.toRadians(initialGeoCoord._1)
     val lon1 = math.toRadians(initialGeoCoord._2)
     
-    def withinBoundary(geoCoord: (Double, Double)): Boolean = {
+    def withinBoundary(geoCoord: (Double, Double)): (Boolean, Double) = {
       val lat2 = math.toRadians(geoCoord._1)
       val lon2 = math.toRadians(geoCoord._2)
       val dlon = math.abs(lat1-lat2)
@@ -27,9 +27,9 @@ object SessionController {
               (math.cos(lat1)*math.cos(lat2)*math.pow(math.sin(dlon/2),2))
       val c = 2*math.atan2(math.sqrt(a), math.sqrt(1-a))
       val distance = 3961*c
-
-      if(distance < 20) true
-      else false
+      
+      val inB: Boolean = distance < 20
+      (inB, distance)
     }
   }
 }
