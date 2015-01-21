266c3044f6da556ba531fc5352dbafa9e1982964
DillonFancher <dfancher@twitter.com>
2015-01-20 18:38:45 -0700

    df: work in progress on qualifier.

--- qualifier/build.sbt
+++ qualifier/build.sbt
@@ -1,7 +1,5 @@
 name := "qualifier"
 
-organization := Common.org
-
 version := "1.0"
 
 version := "0.0.1-SNAPSHOT"
@@ -11,8 +9,9 @@ scalaVersion := "2.10.3"
 libraryDependencies ++= Seq(
    "com.twitter" %% "finatra" % "1.5.2",
     "com.amazonaws" % "aws-java-sdk" % "1.9.9",
-    "com.twitter" %% "finagle-http" % "6.22.0",
-    "com.fasterxml.jackson.core" % "jackson-databind" % "2.4.4"
+    "com.twitter" %% "finagle-http" % "6.11.1",
+    "com.fasterxml.jackson.core" % "jackson-databind" % "2.2.4",
+    "org.json4s"  %% "json4s-jackson" % "3.2.4"
 )
 
 resolvers += "Twitter" at "http://maven.twttr.com"
--- qualifier/src/main/scala/com/weshare/qualifier/Main.scala
+++ qualifier/src/main/scala/com/weshare/qualifier/Main.scala
@@ -1,21 +1,30 @@
 package com.weshare.qualifier
 
 import com.fasterxml.jackson.databind.JsonNode
-import com.fasterxml.jackson.databind._
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.google.common.base.Charsets
 import com.twitter.finagle.{Http, Service}
 import com.twitter.util.{Future, Await}
 import org.jboss.netty.handler.codec.http._
 
 object Main {
-   
+
+  val mapper = new ObjectMapper()
+
   def main(args: Array[String]) {
     val service = new Service[HttpRequest, HttpResponse] {
-      def apply(request: HttpRequest): Future[HttpResponse] = { 
-	      val requestBody: String = request.getContent.toString(Charsets.UTF_8)
-        val requestJson: JsonNode = parseTree(requestBody)
+      def apply(request: HttpRequest): Future[HttpResponse] = {
+	      val requestJson: JsonNode = mapper.readTree(request.getContent.toString(Charsets.UTF_8))
+        val sessionAndGeo: (String, (String, String)) = parseSessionAndGeo(requestJson)
+        val newBoundary: Boundary = Boundary(sessionAndGeo._1, sessionAndGeo._2)
+        getPhonesInSession(sessionAndGeo._1).map( m =>
+          if(newBoundary.withinBoundary(m._2)) {
+            println("This phone, " + m._1 + " is within the boundary, send it the picture.")
+            //handleResponse(sendPicture(newBoundary.pictureId, m._1))
+          }
+        )
         println("The Request Body Contained: " + request.getContent.toString(Charsets.UTF_8))
-	      Future.value(new DefaultHttpResponse(request.getProtocolVersion, HttpResponseStatus.OK))
+        Future.value(new DefaultHttpResponse(request.getProtocolVersion, HttpResponseStatus.OK))
       }
     }
 
@@ -28,22 +37,60 @@ object Main {
     //else println("You are no longer in the session, because you are " + inBoundary._2+ " miles away from DEATH:!")
   }
 
-  case class Boundary(initialGeoCoord: (Double, Double)) {    val lat1 = math.toRadians(initialGeoCoord._1)
-    val lon1 = math.toRadians(initialGeoCoord._2)
-    
-    def withinBoundary(geoCoord: (Double, Double)): (Boolean, Double) = {
+  /**
+   * Keep this in a Tuple 3 for now
+   */
+  def parseSessionAndGeo(picJson: JsonNode): (String, (String, String)) = {
+    (picJson.get("sessionToken").toString, (picJson.get("lat").toString, picJson.get("lon").toString))
+  }
+
+  /**
+   * This needs to be a mysql lookup for the current geo location of every phone in the session
+   * logic: if geo location is being recorded then the phone is in the session otherwise, if it is not updating
+   * geo information we flag the phone table for the corresponding phone and say they are no longer in the session.
+   */
+  def getPhonesInSession(sessionToken: String): List[(String, (Double, Double))] = {
+    List(("1", (1.1, 1.1)), ("2",(1.1, 1.1)), ("3", (1.1, 1.1)), ("4", (1.1, 1.1)))
+  }
+
+  def sendPicture(pictureId: String, phoneId: String): Future[HttpResponse] = {
+    ???
+  }
+
+  def handleResponse(response: Future[HttpResponse]): Future[HttpResponse] = {
+    response.onSuccess( s =>
+      println("fuck ya")
+    ).onFailure( f =>
+      println("fuck no")
+    )
+  }
+
+/**
+ * Sample String JSON Body for picture data:
+ * '{"sessionToken":"abcd1234", "lat":"-90.09", "lon":"90.09"}'
+ * Each picture will need two identifiers to be unique, the sessionToken and the pictureId which will start at 0 and go up
+ * from there, i.e.
+ *    pictureIds = <sessionToken>-0, <sessionToken>-1, <sessionToken>-2, ...
+ */
+
+  case class Boundary(sessionToken: String, initialGeoCoord: (String, String)) {
+    val lat1 = math.toRadians(initialGeoCoord._1.toDouble)
+    val lon1 = math.toRadians(initialGeoCoord._2.toDouble )
+    val pictureId = sessionToken + "1"
+
+  def withinBoundary(geoCoord: (Double, Double)): (Boolean) = {
       val lat2 = math.toRadians(geoCoord._1)
       val lon2 = math.toRadians(geoCoord._2)
       val dlon = math.abs(lat1-lat2)
       val dlat = math.abs(lon1-lon2)
-      
-      val a = (math.pow(math.sin(dlat/2),2)) + 
+
+      val a = math.pow(math.sin(dlat / 2), 2) +
               (math.cos(lat1)*math.cos(lat2)*math.pow(math.sin(dlon/2),2))
       val c = 2*math.atan2(math.sqrt(a), math.sqrt(1-a))
       val distance = 3961*c
-      
+
       val inB: Boolean = distance < 20
-      (inB, distance)
+      inB
     }
   }
 }
