992deab9fd5f7e4497c21271ce368c7a40ce0270
DillonFancher <dfancher@twitter.com>
2015-02-05 10:30:38 -0700

    df: added in user id to user locations, and fixed typo.

--- servicer/src/main/scala/com/weshare/servicer/schema/wesharehier/table/UserLocationsTable.scala
+++ servicer/src/main/scala/com/weshare/servicer/schema/wesharehier/table/UserLocationsTable.scala
@@ -7,8 +7,9 @@ import scala.slick.driver.H2Driver.simple._
 
 class UserLocationsTable(tag: Tag) extends Table[UserLocationRow](tag, "UserLocations") {
   def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
+  def userId = column[Int]("user_id", O.NotNull)
   def lat = column[Float]("lat", O.NotNull)
-  def lon = column[Float]("lno", O.NotNull)
+  def lon = column[Float]("lon", O.NotNull)
   def createdAt = column[Timestamp]("created_at", O.NotNull)
   def * = (id, lat,lon, createdAt) <> (UserLocationRow.tupled, UserLocationRow.unapply)
 }
